2c589c0b5722c042dda3353206120b99a4239328
Christopher Kim <chris.kim1990@gmail.com>
2021-02-12 23:22:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   find_valid_calls.select do |call|
-    call.include?
+    call
   end
-  call
 end
